bc64f942aa7b10e522a868b1e4506e389bca9a31
Субханкулова Камилла <kamillasubhankulova38@gmail.com>
2025-01-17 18:32:28 +0500

    выполнено задание номер 2.

---
+++ Task_02_02/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Data;
+
+namespace Task_02_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 8;
+            double b = 14;
+            double c = Math.PI / 4;
+            var k = Math.Pow(b + Math.Pow(a - 1, 1 / 3), 1 / 4);
+            var l = Math.Abs(a - b) * Math.Pow(Math.Sin(c), 2) + Math.Tan(c);
+            if (l != 0)
+            {
+                var m = k / l;
+                Console.WriteLine(m);
+            }
+            else
+            {
+                Console.WriteLine("Ошибка: деление на ноль!");
+            }
+
+
+
+
+
+        }
+    }
+}
