6b50bdbf10fb0321cdddbbe7a27a3145d2c62b33
Субханкулова Камилла <kamillasubhankulova38@gmail.com>
2025-01-18 19:37:16 +0500

    выполнена задача 2 урока 2

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите свой год рождения: ");
+int year = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите свой месяц рождения: ");
+int month = int.Parse(Console.ReadLine());
+Console.WriteLine("Ввелите дату рождения: ");
+int day = int.Parse(Console.ReadLine());
+DateTime today = DateTime.Today;
+DateTime birthday = new DateTime(year, month, day);
+int age = today.Year - birthday.Year;
+{
+    if (birthday > today.AddYears(age)) age--;
+    if (age >= 18) ;
+    Console.WriteLine("Совершеннолетний");
+                else
+        Console.WriteLine("Не совершеннолетний");
+}
