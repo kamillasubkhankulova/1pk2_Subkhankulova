f37d952b02e7a49bfbdff3511a246a24b0af4653
Субханкулова Камилла <kamillasubhankulova38@gmail.com>
2025-01-18 09:21:01 +0500

    задание 2. урок.2

--- Task_02_02/Program.cs
+++ Task_02_02/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Data;
+using System.Numerics;
 
 namespace Task_02_02
 {
@@ -6,25 +7,12 @@ namespace Task_02_02
     {
         static void Main(string[] args)
         {
-            double a = 8;
-            double b = 14;
-            double c = Math.PI / 4;
-            var k = Math.Pow(b + Math.Pow(a - 1, 1 / 3), 1 / 4);
-            var l = Math.Abs(a - b) * Math.Pow(Math.Sin(c), 2) + Math.Tan(c);
-            if (l != 0)
-            {
-                var m = k / l;
-                Console.WriteLine(m);
-            }
-            else
-            {
-                Console.WriteLine("Ошибка: деление на ноль!");
-            }
-
-
-
-
-
+            double a=8;
+            double b=14;
+            double c=Math.PI/4;
+            double result=Math.Pow(b+Math.Pow(a-1,1.0/3),0.25)/(Math.Abs(a-b)*(Math.Pow(Math.Sin(c),2)+Math.Tan(c)));         
+            Console.WriteLine(result);
+        
         }
     }
 }
